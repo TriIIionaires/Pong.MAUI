7436ba8c7f43f1b0b17d277e9a32ab124745014b
Jadon Malvar <1057627@student.auhsd.us>
2025-04-03 11:55:11 -0700

    Add project files.

---
+++ Pong.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pong.MAUI
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Pong.MAUI/wwwroot/js/Home.js
@@ -0,0 +1,12 @@
+﻿window.SetFocusToElement = function(id) {
+    document.getElementById(id).focus();
+}
+
+window.MovePaddle = function(id, position) {
+    document.getElementById(id).style.top = position.toString() + "%";
+}
+
+window.MoveBall = function (x, y) {
+    document.getElementById("ball").style.top = y.toString() + "%";
+    document.getElementById("ball").style.left = x.toString() + "%";
+}
